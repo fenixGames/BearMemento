c84bc8b603b9dcb844f26eb474c0986b36cf2db5
lara <larafercue@gmail.com>
2020-02-08 15:05:32 +0100

    Add Inventory and pickup objects scripts

---
+++ Assets/Scripts/PickupObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObjects : MonoBehaviour
+{
+    public Inventory inventory;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            inventory.AddObject(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
